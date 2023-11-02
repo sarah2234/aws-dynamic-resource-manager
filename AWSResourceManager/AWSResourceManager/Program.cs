30d9f2fd2c59be6d0adbfd27f36b45e011a884d9
sarah2234 <2234sarah@gmail.com>
2023-11-02 23:02:19 +0900

    feat: add aws console app - get lists of ec2 instances - print information of each ec2 instance

---
+++ AWSResourceManager/AWSResourceManager/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading.Tasks;
+using Amazon;
+using Amazon.EC2;
+using Amazon.EC2.Model;
+using Amazon.Runtime;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        const string accessKey = "";
+        const string secretAccessKey = "";
+
+        var credentials = new BasicAWSCredentials(accessKey, secretAccessKey);
+
+        using var ec2Client = new AmazonEC2Client(credentials, RegionEndpoint.APNortheast2);
+
+        var request = new DescribeInstancesRequest();
+
+        // EC2 인스턴스 리스트 가져오기
+        var response = await ec2Client.DescribeInstancesAsync(request);
+
+        // 인스턴스 정보 출력
+        foreach (var reservation in response.Reservations)
+        {
+            foreach (var instance in reservation.Instances)
+            {
+                Console.WriteLine($"Instance ID: {instance.InstanceId}, State: {instance.State.Name}");
+            }
+        }
+    }
+}
