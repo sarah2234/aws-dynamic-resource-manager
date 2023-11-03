bac277bdfe0c442f7ff9dfd0832604a6fad35f6f
sarah2234 <2234sarah@gmail.com>
2023-11-03 21:22:07 +0900

    "feat: get aws credentials using csv file - csv file format: original file which can be downloaded on AWS"

--- AWSResourceManager/AWSResourceManager/Program.cs
+++ AWSResourceManager/AWSResourceManager/Program.cs
@@ -9,25 +9,40 @@ class Program
 {
     static async Task Main(string[] args)
     {
-        const string accessKey = "";
-        const string secretAccessKey = "";
+        string accessKeyFilePath = @"E:\2020039075_accessKeys.csv";
 
-        var credentials = new BasicAWSCredentials(accessKey, secretAccessKey);
-
-        using var ec2Client = new AmazonEC2Client(credentials, RegionEndpoint.APNortheast2);
-
-        var request = new DescribeInstancesRequest();
-
-        // EC2 인스턴스 리스트 가져오기
-        var response = await ec2Client.DescribeInstancesAsync(request);
-
-        // 인스턴스 정보 출력
-        foreach (var reservation in response.Reservations)
+        try
         {
-            foreach (var instance in reservation.Instances)
+            string content = File.ReadAllText(accessKeyFilePath);
+
+            content = content.Substring(content.IndexOf("\n") + 1);
+
+            string[] keys = content.Split(',');
+            keys[0] = keys[0].Trim();
+            keys[1] = keys[1].Trim();
+
+            var credentials = new BasicAWSCredentials(keys[0], keys[1]);
+
+            using var ec2Client = new AmazonEC2Client(credentials, RegionEndpoint.APNortheast2);
+
+            var request = new DescribeInstancesRequest();
+
+            // EC2 인스턴스 리스트 가져오기
+            var response = await ec2Client.DescribeInstancesAsync(request);
+
+            // 인스턴스 정보 출력
+            foreach (var reservation in response.Reservations)
             {
-                Console.WriteLine($"Instance ID: {instance.InstanceId}, State: {instance.State.Name}");
+                foreach (var instance in reservation.Instances)
+                {
+                    Console.WriteLine($"Instance ID: {instance.InstanceId}, State: {instance.State.Name}");
+                }
             }
         }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+        }
+
     }
 }
